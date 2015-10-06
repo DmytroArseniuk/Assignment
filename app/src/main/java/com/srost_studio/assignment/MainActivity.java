8c68ea44d47307f0f849c1aafe6d852b868854a8
DmytroArseniuk <dmytro.arseniuk@teamdev.com>
2015-10-06 20:02:14 +0300

    Modify MainActivity for service test.

--- app/src/main/java/com/srost_studio/assignment/MainActivity.java
+++ app/src/main/java/com/srost_studio/assignment/MainActivity.java
@@ -1,22 +1,24 @@
 package com.srost_studio.assignment;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import br.com.condesales.EasyFoursquare;
-import br.com.condesales.EasyFoursquareAsync;
-import br.com.condesales.listeners.AccessTokenRequestListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.squareup.otto.Subscribe;
+import com.srost_studio.assignment.events.VenuesFetchedEvent;
+import com.srost_studio.assignment.util.EventBus;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private EasyFoursquareAsync fq;
+    private GoogleApiClient googleApiClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +27,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fq = new EasyFoursquareAsync(this);
-        fq.requestAccess(new AccessTokenRequestListener() {
-            @Override
-            public void onAccessGrant(String accessToken) {
-                Log.d("onAccessGrant", accessToken);
-            }
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
-            @Override
-            public void onError(String errorMsg) {
-                Log.d("onError", errorMsg);
-            }
-        });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EventBus.getInstance().register(this);
+        final Location centerOfNY = new Location("");
+        centerOfNY.setLatitude(40.7406699);
+        centerOfNY.setLongitude(-73.9886812);
+
+        googleApiClient.connect();
+
+
+    }
+
+    @Subscribe
+    public void accept(VenuesFetchedEvent event) {
+        Log.d("RESTOURANT COUNTER", "" + event.getVenues().size());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        if (lastLocation != null) {
+            final Intent intent = new Intent(MainActivity.this, PizzaVenueService.class);
+            intent.putExtra(PizzaVenueService.LATITUDE_TAG, lastLocation.getLatitude());
+            intent.putExtra(PizzaVenueService.LONGITUDE_TAG, lastLocation.getLongitude());
+            startService(intent);
+            Log.d("LAT ", String.valueOf(lastLocation.getLatitude()));
+            Log.d("LON ", String.valueOf(lastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EventBus.getInstance().unregister(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("onConnectionSuspended", "value: " + i);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("onConnectionFailed", connectionResult.toString());
     }
 }
